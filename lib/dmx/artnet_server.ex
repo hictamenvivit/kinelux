2e98fea8ff92c87596b39f3d9f751164242837f0
Maxime Bettinelli <maxime.bettinelli@gmail.com>
2025-11-28 14:37:08 +0800

    misc

--- lib/dmx/artnet_server.ex
+++ lib/dmx/artnet_server.ex
@@ -34,22 +34,26 @@ defmodule DMX.ArtnetServer do
     dmx_value_speed = scale(position.speed * 100, 0, 60)
     dmx_value_x = scale(position.x, -400, 400)
 
-    IO.puts("Speed * 100 =  #{position.speed * 100}, dmx_value = #{dmx_value_speed} ")
+    # IO.puts("Speed * 100 =  #{position.speed * 100}, dmx_value = #{dmx_value_speed} ")
+    IO.puts(seq)
 
     mapping = %{
-      201 => 0,
+      2 => dmx_value_speed,
+      9 => dmx_value_speed,
+      16 => dmx_value_speed,
       202 => 0
       # 202 => scale(speedX * 100, 0, 125)
     }
 
-    frame = DMX.ArtnetFrame.build_frame(mapping, seq)
+    frame = DMX.ArtnetFrame.build_frame(mapping, seq, 1)
     # write_to_file("DATA", frame)
     # TODO: retrieve port properly
     # TODO: retrieve address properly
     # IO.puts("sending frame #{message}")
-    :gen_udp.send(socket, {2, 0, 0, 1}, 6454, frame)
+    :gen_udp.send(socket, {127, 0, 0, 1}, 6454, frame)
+    # IO.puts(frame)
 
-    {:noreply, state}
+    {:noreply, %{socket: socket, seq: rem(seq + 1, 255)}}
   end
 
   defp scale(value, minimum \\ 0, maximum \\ 100) do
